9c96a27bd0050974719f9d1afdf9fb9bb1628fb6
rpayyavula <rpayyavula@ARI-CPU-204.arigs.local>
2022-03-02 18:42:23 +0530

    commit

---
+++ MovieSeatBooking/src/main/java/com/dao/SeatsDao.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+import java.util.List;
+
+import com.entity.Seats;
+
+public interface SeatsDao {
+
+	public boolean insert(Seats seat);
+
+	public List<Seats> getAllSeats();
+
+//	public boolean update(Seats seat);
+
+	public Seats getSeat_No(int seat_no);
+
+}
